824477e5554ec6592b71d145e4be38643b8adbe7
zlayine <zouheirlen@gmail.com>
2023-01-22 20:18:03 +0100

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origins: ["http://localhost:5173"],
+    origins: [process.env.CORS_URL],
   },
 });
 
